e36b3fd270bbb57ac72ea48f499d471f067ff862
Alex Macleod <alex@macleod.io>
2024-02-28 17:46:28 +0000

    rustc

--- tests/sigabrt.rs
+++ tests/sigabrt.rs
@@ -1,27 +1,17 @@
 #![feature(lazy_cell)]
 
-use std::env;
 use std::process::Command;
+use std::thread;
 
 #[test]
 fn check() {
-    println!("{:?}", std::thread::available_parallelism());
+    println!("{:?}", thread::available_parallelism());
 
-    let current_exe_path = env::current_exe().unwrap();
-    let deps_path = current_exe_path.parent().unwrap();
-    let profile_path = deps_path.parent().unwrap();
-
-    std::thread::scope(|s| {
-        for t in 0..std::thread::available_parallelism().unwrap().get() {
-            let program = profile_path.join(if cfg!(windows) {
-                "clippy-driver.exe"
-            } else {
-                "clippy-driver"
-            });
-
+    thread::scope(|s| {
+        for t in 0..thread::available_parallelism().unwrap().get() {
             s.spawn(move || {
                 for i in 0..5000 {
-                    let mut c = Command::new(&program);
+                    let mut c = Command::new("rustc");
                     c.arg("tests/ui/hello_world.rs");
                     c.arg("--out-dir=target/ui");
                     let out = c.output().unwrap();
