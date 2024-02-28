3b60c9c175a9a56503a3ec11580fb01f1c845af7
Alex Macleod <alex@macleod.io>
2024-02-28 20:34:51 +0000

    No ICE hook

--- src/driver.rs
+++ src/driver.rs
@@ -25,28 +25,14 @@ struct ClippyCallbacks;
 
 impl rustc_driver::Callbacks for ClippyCallbacks {}
 
-const BUG_REPORT_URL: &str = "https://github.com/rust-lang/rust-clippy/issues/new?template=ice.yml";
-
-#[allow(clippy::too_many_lines)]
-#[allow(clippy::ignored_unit_patterns)]
 pub fn main() {
     let early_dcx = EarlyDiagCtxt::new(ErrorOutputType::default());
 
     rustc_driver::init_rustc_env_logger(&early_dcx);
 
-    let using_internal_features = rustc_driver::install_ice_hook(BUG_REPORT_URL, |handler| {
-        // FIXME: this macro calls unwrap internally but is called in a panicking context!  It's not
-        // as simple as moving the call from the hook to main, because `install_ice_hook` doesn't
-        // accept a generic closure.
-        let version_info = rustc_tools_util::get_version_info!();
-        handler.note(format!("Clippy version: {version_info}"));
-    });
-
     exit(rustc_driver::catch_with_exit_code(move || {
         let args: Vec<String> = env::args().collect();
 
-        rustc_driver::RunCompiler::new(&args, &mut ClippyCallbacks {})
-            .set_using_internal_features(using_internal_features)
-            .run()
+        rustc_driver::RunCompiler::new(&args, &mut ClippyCallbacks {}).run()
     }))
 }
